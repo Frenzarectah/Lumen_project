f7ac105e020d1610fbe890ce9052095e64342ac7
Frenz <francesco.sorrentino@grupposinergia.com>
2023-09-13 16:31:24 +0200

    transferring project on github

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Author;
+use App\Traits\ApiResponser;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    use ApiResponser;
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+    public function index()
+    {
+        $authors = Author::all();
+        return $this->successResponse($authors);
+    }
+    public function store(Request $request)
+    {
+
+    }
+    public function show($author)
+    {
+
+    }
+    public function update(Request $request, $author)
+    {
+
+    }
+    public function destroy($author)
+    {
+
+    }
+}
